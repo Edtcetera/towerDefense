ebfbf5091195a45c64a6f5b4c21f3b2839836b2c
Edward <edwardcai@outlook.com>
2016-09-21 19:08:32 -0700

    added some Sequential Timeline animations for enemy sprite

--- src/sprite/Enemy.java
+++ src/sprite/Enemy.java
@@ -1,7 +1,12 @@
 package sprite;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.SequentialTransition;
+import javafx.animation.Timeline;
 import javafx.scene.Group;
 import javafx.scene.image.Image;
+import javafx.util.Duration;
 import tile.Node;
 import tile.Tile;
 
@@ -32,6 +37,31 @@ public class Enemy extends Sprite{
         super.getIv().setPreserveRatio(true);
         super.getIv().relocate(x,y);
         spriteGroup.getChildren().add(super.getIv());
+
+
+        //TODO: refactor this and make it better
+        //Example timeline sequential animation
+        final Timeline timeline1 = new Timeline();
+        final Timeline timeline2 = new Timeline();
+        final Timeline timeline3 = new Timeline();
+
+        final KeyValue x1 = new KeyValue(super.getIv().xProperty(), 0*TILE_SIZE);
+        final KeyValue y1 = new KeyValue(super.getIv().yProperty(), 7*TILE_SIZE);
+        final KeyFrame kf1 = new KeyFrame(Duration.seconds(3), x1, y1);
+
+        final KeyValue x2 = new KeyValue(super.getIv().xProperty(), 6*TILE_SIZE);
+        final KeyFrame kf2 = new KeyFrame(Duration.seconds(2), x2);
+
+        final KeyValue y3 = new KeyValue(super.getIv().yProperty(), 15*TILE_SIZE);
+        final KeyFrame kf3 = new KeyFrame(Duration.seconds(3), y3);
+
+        timeline1.getKeyFrames().add(kf1);
+        timeline2.getKeyFrames().add(kf2);
+        timeline3.getKeyFrames().add(kf3);
+        SequentialTransition sequence = new SequentialTransition(timeline1, timeline2, timeline3);
+        sequence.setCycleCount(5);
+        sequence.play();
+        //======================
     }
 
     private boolean setFacingDirection() {
